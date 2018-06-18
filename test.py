029b9fe3c10bc6c7eacc44128c8fafcd53ad9128
Walter Kuppens <reshurum@gmail.com>
2018-06-18 12:50:30 -0700

    Finished basic testing script

--- test.py
+++ test.py
@@ -22,18 +22,22 @@
 
 import glob
 import os
+import subprocess
 
 MLBF_PATH = './builddir/mlbf'
 MLBF_TEST_DIR = './tests'
 
 
 def test_mlbf():
-    """Executes all the examples"""
+    """Executes all brainfuck test scripts and tests their output."""
 
     if not mlbf_exists():
         raise RuntimeError("mlbf hasn't been compiled yet.")
 
     scripts = map(lambda x: x, glob.glob(os.path.join(MLBF_TEST_DIR, '*.b')))
+
+    for script in scripts:
+        test_script(*generate_script_names(script))
 
 
 def generate_script_names(fpath):
@@ -42,17 +46,49 @@ def generate_script_names(fpath):
     if extension != '.b':
         raise ValueError('Only pass brainfuck source code to this function.')
 
-    return {
-        'source': '{}.b'.format(filename),
-        'input': '{}.b.in'.format(filename),
-        'output': '{}.b.out'.format(filename),
-    }
+    return (
+        '{}.b'.format(filename),
+        '{}.b.in'.format(filename),
+        '{}.b.out'.format(filename),
+    )
 
 
 def mlbf_exists():
     """Ensures mlbf has been compiled and is available."""
 
     return os.path.isfile(MLBF_PATH)
+
+
+def test_script(source, input, output):
+    """Executes a brainfuck script and tests the output."""
+
+    if not os.path.isfile(source):
+        raise RuntimeError("Source file '{}' doesn't exist.".format(source))
+
+    stdin = subprocess.DEVNULL
+
+    try:
+        if os.path.isfile(input):
+            stdin = open(input, 'rb')
+
+        pipe = subprocess.Popen(
+            [MLBF_PATH, source],
+            stdin=stdin,
+            stdout=subprocess.PIPE)
+        data = pipe.communicate()[0]
+
+        if pipe.returncode != 0:
+            raise RuntimeError("Got non-zero exit code ({}) from mlbf.".format(pipe.returncode))
+
+        if os.path.isfile(output):
+            with open(output, 'rb') as f:
+                expected_data = f.read()
+                if expected_data != data:
+                    raise RuntimeError("Expected -> {}, Got -> {}".format(
+                        expected_data, data))
+    finally:
+        if stdin != subprocess.DEVNULL:
+            stdin.close()
 
 
 if __name__ == '__main__':
